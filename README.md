16d4d47cd74a208d34e6043c51f68ae3ab2b9565
Melissa Hoffman <melissa.hoffman8@gmail.com>
2018-04-02 14:52:03 -0400

    Added combination script, updated README. 

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GBT_ALMA_Combination
 GBT &amp; ALMA Image Combination
+
+This is a CASA script that combines a GBT total power (TP) image and an
+ALMA interferometric image. It also smooths the GBT image to the ALMA
+12-m TP and combines this with the ALMA image to simulate an ALMA only
+combination. 
---
+++ combination.py
@@ -0,0 +1,147 @@
+# Combination script for GBT + ALMA Data
+# At the end we smooth ALMA to GBT and feather that to 
+# simulate a regular TP+ALMA combination
+
+# Use imhead to determine what need to be regridded/transformed
+mykeys=['cdelt1','cdelt2','cdelt3','cdelt4','restfreq']
+
+for f in myfiles:
+     print(f)
+     print('------------')
+     for key in mykeys:
+         q = imhead('flagged_image_r_2.0.image',mode='get',hdkey=key)
+         print(str(key)+' : '+str(q))
+'''
+flagged_image_r_2.0.image/
+------------
+cdelt1 : {'value': -2.42406840554768e-06, 'unit': 'rad'}
+cdelt2 : {'value': 2.42406840554768e-06, 'unit': 'rad'}
+cdelt3 : {'value': 1.0, 'unit': ''}
+cdelt4 : {'value': -28661.91616821289, 'unit': 'Hz'}
+restfreq : {'value': 85926263000.0, 'unit': 'Hz'}
+flagged_image_r_2.0.pb/
+------------
+cdelt1 : {'value': -2.42406840554768e-06, 'unit': 'rad'}
+cdelt2 : {'value': 2.42406840554768e-06, 'unit': 'rad'}
+cdelt3 : {'value': 1.0, 'unit': ''}
+cdelt4 : {'value': -28661.91616821289, 'unit': 'Hz'}
+restfreq : {'value': 85926263000.0, 'unit': 'Hz'}
+PerBolo58_NH2D_gridder_v2_cube_Jybeam.image/
+------------
+cdelt1 : {'value': -2.42406840554768e-06, 'unit': 'rad'}
+cdelt2 : {'value': 2.42406840554768e-06, 'unit': 'rad'}
+cdelt3 : {'value': 1.0, 'unit': ''}
+cdelt4 : {'value': -28661.91616821289, 'unit': 'Hz'}
+restfreq : {'value': 85926263000.0, 'unit': 'Hz'}
+
+'''
+
+# In your headers, there are three things to note: 
+# 1. Axes
+# 2. Order of Axes
+# 3. Rest Frequency 
+# As long as these match, or as long as we can make them 
+# match we shouldn't run into any problems when feathering. 
+
+# Regrid GBT Image to match ALMA image
+
+imregrid(imagename='PerBolo58_NH2D_gridder_v2_cube_Jybeam.image',
+         template='flagged_image_r_2.0.image',
+         axes=[0, 1],
+         output='GBT.image.regrid')
+
+# Reorder the axes of the GBT to match ALMA/pb 
+
+imtrans(imagename='GBT.image.regrid',
+        outfile='GBT.image.regrid.ro',
+        order='013-2')
+
+
+# Trim all images to the same size 
+# Note: This step is not necessary
+
+imsubimage(imagename='GBT.image.regrid.ro',
+           outfile='GBT.image.regrid.ro.subim',
+           box='143,143,366,366')
+
+imsubimage(imagename='flagged_image_r_2.0.image',
+           outfile='ALMA.image.subim',
+           box='143,143,366,366')
+
+imsubimage(imagename='flagged_image_r_2.0.pb',
+           outfile='pb.subim', 
+           box='143,143,366,366')
+
+
+
+# trim extra channels off of GBT data
+
+imsubimage(imagename='GBT.image.regrid.ro.subim',
+           outfile='GBT.image.regrid.ro.subim.trim',
+           chans='89~288')
+
+
+# Multiply the flux by the GBT image to get the same response
+
+immath(imagename=['GBT.image.regrid.ro.subim.trim',
+                  'pb.subim'],
+       expr='IM0*IM1',
+       outfile='GBT.multiplied')
+
+
+# Feather together the GBT*pb and ALMA images
+
+feather(imagename='Feather.image',
+        highres='ALMA.image.subim',
+        lowres='GBT.multiplied')
+
+###############################################################
+# Copy & smooth GBT image to look like TP 
+# for 85.9 GHz and 12-m dish size, TP beam will be ~73" 
+
+os.system('cp -r PerBolo58_NH2D_gridder_v2_cube_Jybeam.image GBT.image')
+mybeam = {'major': '73.2arcsec', 'minor': '73.2arcsec', 'pa': '0deg'}  
+imsmooth( imagename='PerBolo58_NH2D_gridder_v2_cube_Jybeam.image', kernel='gauss', beam=mybeam, targetres=True,outfile='GBT.smooth') 
+
+# Regrid to ALMA image, as before 
+
+imregrid(imagename='GBT.smooth',
+         template='flagged_image_r_2.0.image',
+         axes=[0, 1],
+         output='GBT.smooth.regrid')
+
+# Still gotta fix that pesky axis...
+
+imtrans(imagename='GBT.smooth.regrid',
+        outfile='GBT.smooth.regrid.ro',
+        order='013-2')
+
+
+# Trim extra channels off of GBT data
+
+imsubimage(imagename='GBT.smooth.regrid.ro',
+           outfile='GBT.smooth.regrid.ro.trim',
+           chans='89~288')
+
+
+# Trim cube to correct size 
+
+imsubimage(imagename='GBT.smooth.regrid.ro.trim',
+           outfile='GBT.smooth.regrid.ro.trim.subim',
+           box='143,143,366,366')
+
+
+# Multiply by primary beam response 
+
+immath(imagename=['GBT.smooth.regrid.ro.trim.subim',
+                  'pb.subim'],
+       expr='IM0*IM1',
+       outfile='GBT.smooth.multiplied')
+
+# Delete the telescope info from the header
+ 
+imhead('GBT.smooth.multiplied',mode='del',hdkey='telescope')
+
+feather(imagename='Feather.smooth.image',
+        highres='ALMA.image.subim',
+        lowres='GBT.smooth.multiplied')
